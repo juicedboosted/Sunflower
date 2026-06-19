4873f4c1eb75c4a10417e3c3eca60a261e54c12d
Mackenzie <mackenzie.smith.nz@gmail.com>
2026-06-19 23:41:48 +1200

    pages set up for health app
    can navigate between home page, health app, prescriptions, booking appointments, and virtual appointment now. also fixed back button so it goes to the previous page in the case of the health app rather than closing the whole app without changing the active canvas.

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -2,14 +2,64 @@ using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
+    [SerializeField] GameObject m_mainPage;
+    [SerializeField] GameObject m_bookingPage;
+    [SerializeField] GameObject m_prescriptionPage;
+    [SerializeField] GameObject m_virtualApptPage;
 
+    [SerializeField] AppManager appManager;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnEnable()
     {
-        
+        OpenMainPage();
     }
 
+    public void OpenMainPage()
+    {
+        m_mainPage.SetActive(true);
+        m_bookingPage.SetActive(false);
+        m_prescriptionPage.SetActive(false);
+    }
 
+    public void OpenBookingPage()
+    {
+        m_mainPage.SetActive(false);
+        m_bookingPage.SetActive(true);
+        m_prescriptionPage.SetActive(false);
+        m_virtualApptPage.SetActive(false);
+    }
 
+    public void OpenPrescPage()
+    {
+        m_mainPage.SetActive(false);
+        m_bookingPage.SetActive(false);
+        m_prescriptionPage.SetActive(true);
+        m_virtualApptPage.SetActive(false);
+    }
+    public void StartAppt()
+    {
+        // only do this function if it is the scheduled appointment time from the calendar
+        m_mainPage.SetActive(false);
+        m_bookingPage.SetActive(false);
+        m_prescriptionPage.SetActive(false);
+        m_virtualApptPage.SetActive(true);
+    }
+
+    // If the health app is open & not on the main page, the back button will go to the main health page rather than closing the app.
+    public void CloseApp()
+    {
+        if (m_bookingPage.activeSelf || m_prescriptionPage.activeSelf)
+        {
+            OpenMainPage();
+        }
+        else if (m_virtualApptPage.activeSelf)
+        {
+            OpenBookingPage();
+        }
+        else
+        {
+            appManager.CloseApp();
+        }
+    }
 }
