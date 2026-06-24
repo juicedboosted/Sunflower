36ef56e4d06d93830fc4b1caa2c4e73dea4cfdba
JessicaHarveyCross <jessieharveycross@gmail.com>
2026-06-24 16:17:49 +1200

    Revert "added jessie's comments"
    This reverts commit 7cb780b49f11bd317020e9998b72cd690bd4dda2, reversing
    changes made to 3388250571fd610e726d578464bd71012a9dcf14.

--- Assets/Scripts/TimeSlot.cs
+++ Assets/Scripts/TimeSlot.cs
@@ -4,13 +4,10 @@ using UnityEngine.EventSystems;
 public class TimeSlot : MonoBehaviour, IDropHandler
 {
    public TrackerManager m_trackerManager;
-
-    //call when task is dropped on calendar time slot
     public void OnDrop(PointerEventData _eventData)
     {
         GameObject taskObject = _eventData.pointerDrag;
 
-        //make sure task actually exists and is valid
         if (taskObject == null)
         {
             return;
@@ -29,13 +26,10 @@ public class TimeSlot : MonoBehaviour, IDropHandler
             return;
         }
 
-        //only spent energy the first time the task is scheduled
         if (dragTask.m_hasSpentEnergy == false)
         {
-            //check if player has enough energy for the task
             if (m_trackerManager.SpendEnergy(dragTask.m_energyCost))
             {
-                //record the task and remove energy
                 m_trackerManager.AddScheduledTask();
                 dragTask.m_hasSpentEnergy = true;
                 MoveTask(taskObject);
@@ -43,16 +37,13 @@ public class TimeSlot : MonoBehaviour, IDropHandler
         }
         else
         {
-            //allow the scheduled tasks to move freely between slots
             MoveTask(taskObject);
         }
     }
 
-//puts task inside time slot
 private void MoveTask(GameObject _taskObject)
     {
         _taskObject.transform.SetParent(transform, false);
-        //centre the task
         _taskObject.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
     }
 }
