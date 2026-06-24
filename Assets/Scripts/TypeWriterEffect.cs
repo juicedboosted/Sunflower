bf9b8453f54d881e65a5158da1ca42d680d90f7f
JessicaHarveyCross <jessieharveycross@gmail.com>
2026-06-24 15:18:41 +1200

    updated comments

--- Assets/Scripts/TypeWriterEffect.cs
+++ Assets/Scripts/TypeWriterEffect.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class TypeWriterEffect : MonoBehaviour
 {
     public TMP_Text m_text;
+    //delay between each character
     public float m_delay = 0.5f;
 
 
@@ -14,11 +15,14 @@ public class TypeWriterEffect : MonoBehaviour
         StartCoroutine(TypeText(_message));
     }
 
+    //reveal one character at a time
     private IEnumerator TypeText(string _message)
     {
+        //set message and hide all characters
         m_text.text = _message;
         m_text.maxVisibleCharacters = 0;
 
+        //reveal characters one by one until full message visible
         while (m_text.maxVisibleCharacters < _message.Length)
         {
             m_text.maxVisibleCharacters++;
